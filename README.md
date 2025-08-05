da78ac0236fac228c962fccb8926f5fd418a423c
Mauricio Santecchia <mauricios@vianet.com.ar>
2025-08-05 19:57:07 -0300

    .

--- README.md
+++ README.md
@@ -1,63 +1,101 @@
 # RList
 
-  RList (Rotating List) allow to work with circular lists. A circular lists is a finite list that
-  can be traversed as if it were infinite. This is possible because in a circular list, it is
-  assumed that a copy of the original list is inserted at the end of the list, and so on infinitely.
+RList (Rotating List) allow to work with circular lists. A circular lists is a finite list that
+can be traversed as if it were infinite. This is possible because in a circular list, it is
+assumed that a copy of the original list is inserted at the end of the list, and so on infinitely.
 
-  Internally a RList is a map that store the original list and a pointer indicating the current
-  position in base 1 (as Erlang lists).
+Internally a RList is a map that store the original list and a pointer indicating the current
+position in base 1 (as Erlang lists).
 
-  Although internally an RList is a map, its visual representation (IO.inspect) is:
+Although internally an RList is a map, its visual representation (IO.inspect) is:
 
-  ```elixir
-  iex> RList.new([1, 2, 3, 4, 5])
-  #RList[1, 2, 3, 4, 5]/1
-  ```
-  The original list and pointer will always be displayed. If you move the pointer the representation
-  look different but internally the original list will be mantained.
+```elixir
+iex> RList.new([1, 2, 3, 4, 5])
+#RList[1, 2, 3, 4, 5]/1
+```
+The original list and pointer will always be displayed. If you move the pointer the representation
+look different but internally the original list will be mantained.
 
-  ```elixir
-  iex> rl = RList.new([1, 2, 3, 4, 5])
-  #RList[1, 2, 3, 4, 5]/1
+```elixir
+iex> rl = RList.new([1, 2, 3, 4, 5])
+#RList[1, 2, 3, 4, 5]/1
 
-  iex> rl2 = RList.forward(rl)
-  #RList[2, 3, 4, 5, 1]/2
+iex> rl2 = RList.forward(rl)
+#RList[2, 3, 4, 5, 1]/2
 
-  iex> RList.equals?(rl, rl2)
-  true
-  ```
-  As you can see, both the list and the pointer differ, but the RLists are equals because their
-  original lists are the same.
+iex> RList.equals?(rl, rl2)
+true
+```
+As you can see, both the list and the pointer differ, but the RLists are equals because their
+original lists are the same.
 
-  It is necessary to clarify what the pointer value means. When the pointer is, for example, 2, it
-  means that the first value in the current list is equivalent to the value with index 2 in the
-  original list (remember, indexes in RList work on a base 1).
+It is important to clarify what the pointer value means. When the pointer is, for example, 2, it
+means that the first value in the current list is equivalent to the value with index 2 in the
+original list (remember, indexes in RList work on a base 1).
 
-  ```elixir
-  ## When pointer is 1 you see the original list
-  #RList[1, 2, 3, 4, 5]/1
-            ^
-            |
-            ·----------------------------------------------·
-                                                           |
-  ## When pointer is not 1 you see the list rotatated      |
-  #RList[2, 3, 4, 5, 1]/2                                  |
-                        |                                  |
-                        ·----------------------------------·
+```elixir
+## When pointer is 1, you see the original list
+#RList[1, 2, 3, 4, 5]/1
+          ^
+          |
+          +----------------------------------------------+
+                                                          |
+## When pointer is not 1, you see the list rotatated      |
+#RList[2, 3, 4, 5, 1]/2                                  |
+                      |                                  |
+                      +----------------------------------+
 
-  ```
+```
 
-  ## Enumerable
+An RList physically stores what we will call the “current list” (i.e., the rotated list) and implicitly 
+what we will call the “original list,” which is the “current list” when the pointer is equal to 1.
 
-  RList implements Enumerable so you can play with them as Streams or use the Enum.* functions.
+```elixir 
+iex> rl = RList.new([1, 2, 3, 4, 5])
+#RList[1, 2, 3, 4, 5]/1 
+## Original list: [1, 2, 3, 4, 5]
+## Current list: [1, 2, 3, 4, 5]
 
-  ```elixir
-  iex> rl = RList.new([1, 2, 3, 4, 5])
-  #RList[1, 2, 3, 4, 5]/1
+iex> RList.forward(rl, 3)
+#RList[4, 5, 1, 2, 3]/4
+## Original list: [1, 2, 3, 4, 5]
+## Current list: [4, 5, 1, 2, 3]
 
-  iex> Enum.take(rl, 20)
-  [1, 2, 3, 4, 5, 1, 2, 3, 4, 5, 1, 2, 3, 4, 5, 1, 2, 3, 4, 5]
-  ```
+```
+
+## Enumerable
+
+RList implements Enumerable so you can play with Streams or use the Enum.* functions.
+
+```elixir
+iex> rl = RList.new([1, 2, 3, 4, 5])
+#RList[1, 2, 3, 4, 5]/1
+
+iex> Enum.take(rl, 20)
+[1, 2, 3, 4, 5, 1, 2, 3, 4, 5, 1, 2, 3, 4, 5, 1, 2, 3, 4, 5]
+
+iex> rl 
+  |> Stream.map(fn v -> v * 3 end) 
+  |> Stream.map(fn v -> v - 1 end) 
+  |> Enum.take(22)
+[2, 5, 8, 11, 14, 2, 5, 8, 11, 14, 2, 5, 8, 11, 14, 2, 5, 8, 11, 14, 2, 5]
+```
+
+Since an RList could be iterated ad infinitum, the Enum.* functions that has not stop condition will 
+take the current list of the RList as enumerable input. 
+
+```elixir
+iex> rl = RList.new([1, 2, 3, 4, 5])
+#RList[1, 2, 3, 4, 5]/1
+
+iex> Enum.map(rl, fn v -> v * 2 end)
+[2, 4, 6, 8, 10]
+
+iex> rl = RList.forward(rl, 3)
+#RList[4, 5, 1, 2, 3]/4
+
+iex> Enum.map(rl, fn v -> v * 2 end)
+[8, 10, 2, 4, 6]
 
 ## Installation
 
--- lib/rlist.ex
+++ lib/rlist.ex
@@ -1,66 +1,6 @@
 defmodule RList do
-  @doc """
-  RList (Rotating List) allow to work with circular lists. A circular lists is a finite list that
-  can be traversed as if it were infinite. This is possible because in a circular list, it is
-  assumed that a copy of the original list is inserted at the end of the list, and so on infinitely.
+  @moduledoc "README.md" |> File.read!()
 
-  Internally a RList is a map that store the original list and a pointer indicating the current
-  position in base 1 (as Erlang lists).
-
-  Although internally an RList is a map, its visual representation (IO.inspect) is:
-
-  ```elixir
-  iex> RList.new([1, 2, 3, 4, 5])
-  #RList[1, 2, 3, 4, 5]/1
-  ```
-  The original list and pointer will always be displayed. If you move the pointer the representation
-  look different but internally the original list will be mantained.
-
-  ```elixir
-  iex> rl = RList.new([1, 2, 3, 4, 5])
-  #RList[1, 2, 3, 4, 5]/1
-
-  iex> rl2 = RList.forward(rl)
-  #RList[2, 3, 4, 5, 1]/2
-
-  iex> RList.equals?(rl, rl2)
-  true
-  ```
-  As you can see, both the list and the pointer differ, but the RLists are equals because their
-  original lists are the same.
-
-  It is necessary to clarify what the pointer value means. When the pointer is, for example, 2, it
-  means that the first value in the current list is equivalent to the value with index 2 in the
-  original list (remember, indexes in RList work on a base 1).
-
-  ```elixir
-  ## When pointer is 1 you see the original list
-  #RList[1, 2, 3, 4, 5]/1
-            ^
-            |
-            ·----------------------------------------------·
-                                                           |
-  ## When pointer is not 1 you see the list rotatated      |
-  #RList[2, 3, 4, 5, 1]/2                                  |
-                        |                                  |
-                        ·----------------------------------·
-
-  ```
-
-  ## Enumerable
-
-  RList implements Enumerable so you can play with them as Streams or use the Enum.* functions.
-
-  ```elixir
-  iex> rl = RList.new([1, 2, 3, 4, 5])
-  #RList[1, 2, 3, 4, 5]/1
-
-  iex> Enum.take(rl, 20)
-  [1, 2, 3, 4, 5, 1, 2, 3, 4, 5, 1, 2, 3, 4, 5, 1, 2, 3, 4, 5]
-  ```
-
-
-  """
   @enforce_keys [ :list, :ptr ]
   defstruct [ :list, :ptr ]
 
@@ -70,7 +10,43 @@ defmodule RList do
     end
   end
 
-  ## When it is a pattern
+  @type t() :: %RList{}
+
+  @doc """
+  Used to match RList in the style of erlang list pattern. Ex:
+  ```elixir
+  defmodule Tests do
+    use RList
+    import RList
+
+    def test(match [h | t]) do
+      IO.puts "H: \#{h}"
+      IO.inspect t, label: "T"
+      :timer.sleep(1000)
+      test(forward t)
+    end
+  end
+
+  rl = RList.new([1,2,3])
+  Tests.test(rl)
+  ## Output:
+  H: 1
+  T: #RList[1, 2, 3]/1
+  H: 2
+  T: #RList[2, 3, 1]/2
+  H: 3
+  T: #RList[3, 1, 2]/3
+  H: 1
+  T: #RList[1, 2, 3]/1
+  H: 2
+  T: #RList[2, 3, 1]/2
+  ...
+  ...
+  ```
+  Combining `match` and `forward/1` you can go through a RList in a very similar way to how you do
+  it with Erlang lists. You must not forget that, since RLists are endless, you must provide an
+  exit condition when appropriate.
+  """
   defmacro match([{:|, _, [h, t]}]) do
     {:=, [],
     [
@@ -93,6 +69,7 @@ defmodule RList do
   end
   defmacro match(list), do: list
 
+  @doc false
   defmacro from_list(list) do
     quote do
       [h | t] = unquote(list)
@@ -112,6 +89,15 @@ defmodule RList do
   ## API
   #################################################################################################
 
+  @doc """
+  Create a new RList from a list or a range. The initial list/range will be the original list and
+  the pointer of the RList will be initialized to 1.
+  ```elixir
+  iex> rl = RList.new([1, 2, 3, 4, 5])
+  #RList[1, 2, 3, 4, 5]/1
+  ```
+  """
+  @spec new(list :: list() | Range.t()) :: RList.t()
   def new(list) when is_list(list) do
     [h | t] = list
     %RList{list: [h | (t ++ [h])], ptr: 1}
@@ -120,20 +106,41 @@ defmodule RList do
     range |> Range.to_list() |> new()
   end
 
+  @doc """
+  Just an alias for `new(range)`.
+  """
+  @spec from_range(range :: Range.t()) :: RList.t()
   def from_range(_.._//_ = range), do: new(range)
 
+  @doc """
+  Return the size of the original list.
+  """
+  @spec size(rlist :: RList.t()) :: non_neg_integer()
   def size(rlist) do
     length(rlist.list) - 1
   end
 
+  @doc """
+  Return the current list as a tuple.
+  """
+  @spec to_tuple(rlist :: RList.t()) :: tuple()
   def to_tuple(rlist) do
     List.to_tuple(to_list(rlist))
   end
 
+  @doc """
+  Reset the pointer of the list to 1 and return the new RList. It is equivalent to call
+  `ptr(rlist, 1)`.
+  """
+  @spec reset(rlist :: RList.t()) :: RList.t()
   def reset(rlist) do
     ptr(rlist, 1)
   end
 
+  @doc """
+  It is the same that call `Enum.take(rlist, count)`.
+  """
+  @spec take(rlist :: RList.t(), count :: non_neg_integer()) :: list()
   def take(_, 0), do: []
   def take(%RList{} = rlist, count) do
     {value, rlist} = next(rlist)
@@ -145,12 +152,41 @@ defmodule RList do
     [ value ] ++ take(%Stream{stream | enum: rlist}, count - 1)
   end
 
+  @doc """
+  Take a RList, extract the first value of the current list and move de pointer 1 unit. Return the
+  value and the new RList.
+  ```elixir
+  iex> rl = RList.new([1, 2, 3, 4, 5])
+  #RList[1, 2, 3, 4, 5]/1
+
+  iex> {value, rl} = RList.next(rl)
+  {1, #RList[2, 3, 4, 5, 1]/2}
+
+  iex> {value, rl} = RList.next(rl)
+  {2, #RList[3, 4, 5, 1, 2]/3}
+  ```
+  """
+  @spec next(rlist :: RList.t()) :: {term(), RList.t()}
   def next(rlist) do
     match([value | _]) = rlist
     new_rlist = forward(rlist)
     {value , new_rlist}
   end
 
+  @doc """
+  Take a RList, move de pointer count units and return the new RList.
+  ```elixir
+  iex> rl = RList.new([1, 2, 3, 4, 5])
+  #RList[1, 2, 3, 4, 5]/1
+
+  iex> rl = RList.forward(rl)
+  #RList[2, 3, 4, 5, 1]/2
+
+  iex> rl = RList.forward(rl, 3)
+  #RList[5, 1, 2, 3, 4]/5
+  ```
+  """
+  @spec forward(rlist :: RList.t(), count :: non_neg_integer() \\ 1) :: RList.t()
   def forward(rlist, count \\ 1)
   def forward(rlist, 0), do: rlist
   def forward(rlist, count) do
@@ -164,11 +200,26 @@ defmodule RList do
   @doc false
   def rotate(rlist), do: forward(rlist)
 
+  @doc """
+  Take a RList and return the current list (not the original list).
+  """
+  @spec to_list(rlist :: RList.t()) :: list()
   def to_list(rlist) do
     :lists.sublist(rlist.list, 1, size(rlist))
   end
 
+  @doc """
+  Return the current value of the pointer.
+  """
+  @spec ptr(rlist :: RList.t()) :: non_neg_integer()
   def ptr(rlist), do: rlist.ptr
+
+
+  @doc """
+  Set the current value of the pointer, adjust the list according to the pointer value and returns
+  the new list.
+  """
+  @spec ptr(rlist :: RList.t(), ptr :: non_neg_integer()) :: RList.t()
   def ptr(%RList{list: list} = rlist, new_ptr) when is_integer(new_ptr) and new_ptr > 0 and new_ptr < length(list) do
     ptr_helper(rlist, new_ptr)
   end
@@ -195,6 +246,10 @@ defmodule RList do
     }
   end
 
+  @doc """
+  Compare 2 RList and return true if both original list are equals or false otherwise.
+  """
+  @spec equals?(left :: RList.t(), right :: RList.t()) :: boolean()
   def equals?(left, right) do
     left |> RList.ptr(1) |> Map.get(:list) == right |> RList.ptr(1) |> Map.get(:list)
   end
@@ -252,13 +307,13 @@ end
 
 
 #################################################################################################
-defmodule Tests2 do
-  use RList
+# defmodule Tests2 do
+#   use RList
 
-  def test(RList.match [h | t]) do
-    IO.puts "H: #{h}"
-    IO.inspect t, label: "T"
-    :timer.sleep(1000)
-    test(RList.forward t)
-  end
-end
+#   def test(RList.match [h | t]) do
+#     IO.puts "H: #{h}"
+#     IO.inspect t, label: "T"
+#     :timer.sleep(1000)
+#     test(RList.forward t)
+#   end
+# end
